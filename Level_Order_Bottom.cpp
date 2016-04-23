2a8eb209fb9ab327d356c32b550c6e36e9c739f1
YangYang <iyyang501@gmail.com>
2016-04-23 16:20:00 -0700

    levelOrderBottom

---
+++ Level_Order_Bottom.cpp
@@ -0,0 +1,37 @@
+#include <queue>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class TreeNode {
+public:
+	int val;
+    TreeNode *left, *right;
+    TreeNode(int val) {
+    	this->val = val;
+        this->left = this->right = NULL;
+    }
+};
+ 
+ 
+ /*
+ * Binary Tree Level Order Traversal _2 : 
+ * 由下向上逐层遍历
+ */
+
+ class Solution{
+ public:
+ 	vector<vector<int>> levelOrderBottom(TreeNode *root){
+ 	
+ 	}
+ };
+
+ int main(){
+ 	// do sth 
+ 	return 0;
+ }
+
+
+
+
